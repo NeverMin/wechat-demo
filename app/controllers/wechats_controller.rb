79f189f7f6ff74be5009abb335cabf09870bf357
NeverMin <never.min@gmail.com>
2018-03-20 23:02:02 +0800

    added wechat echo

---
+++ app/controllers/wechats_controller.rb
@@ -0,0 +1,130 @@
+class WechatsController < ActionController::Base
+  wechat_responder
+
+  # default text responder when no other match
+  on :text do |request, content|
+    request.reply.text "echo: #{content}" # Just echo
+  end
+
+  # When receive 'help', will trigger this responder
+  on :text, with: 'help' do |request|
+    request.reply.text 'help content'
+  end
+
+  # When receive '<n>news', will match and will get count as <n> as parameter
+  on :text, with: /^(\d+) news$/ do |request, count|
+    # Wechat article can only contain max 8 items, large than 8 will be dropped.
+    news = (1..count.to_i).each_with_object([]) { |n, memo| memo << { title: 'News title', content: "No. #{n} news content" } }
+    request.reply.news(news) do |article, n, index| # article is return object
+      article.item title: "#{index} #{n[:title]}", description: n[:content], pic_url: 'http://www.baidu.com/img/bdlogo.gif', url: 'http://www.baidu.com/'
+    end
+  end
+
+  on :event, with: 'subscribe' do |request|
+    request.reply.text "#{request[:FromUserName]} subscribe now"
+  end
+
+  # When unsubscribe user scan qrcode qrscene_xxxxxx to subscribe in public account
+  # notice user will subscribe public account at the same time, so wechat won't trigger subscribe event anymore
+  on :scan, with: 'qrscene_xxxxxx' do |request, ticket|
+    request.reply.text "Unsubscribe user #{request[:FromUserName]} Ticket #{ticket}"
+  end
+
+  # When subscribe user scan scene_id in public account
+  on :scan, with: 'scene_id' do |request, ticket|
+    request.reply.text "Subscribe user #{request[:FromUserName]} Ticket #{ticket}"
+  end
+
+  # When no any on :scan responder can match subscribe user scanned scene_id
+  on :event, with: 'scan' do |request|
+    if request[:EventKey].present?
+      request.reply.text "event scan got EventKey #{request[:EventKey]} Ticket #{request[:Ticket]}"
+    end
+  end
+
+  # When enterprise user press menu BINDING_QR_CODE and success to scan bar code
+  on :scan, with: 'BINDING_QR_CODE' do |request, scan_result, scan_type|
+    request.reply.text "User #{request[:FromUserName]} ScanResult #{scan_result} ScanType #{scan_type}"
+  end
+
+  # Except QR code, wechat can also scan CODE_39 bar code in enterprise account
+  on :scan, with: 'BINDING_BARCODE' do |message, scan_result|
+    if scan_result.start_with? 'CODE_39,'
+      message.reply.text "User: #{message[:FromUserName]} scan barcode, result is #{scan_result.split(',')[1]}"
+    end
+  end
+
+  # When user clicks the menu button
+  on :click, with: 'BOOK_LUNCH' do |request, key|
+    request.reply.text "User: #{request[:FromUserName]} click #{key}"
+  end
+
+  # When user views URL in the menu button
+  on :view, with: 'http://wechat.somewhere.com/view_url' do |request, view|
+    request.reply.text "#{request[:FromUserName]} view #{view}"
+  end
+
+  # When user sends an image
+  on :image do |request|
+    request.reply.image(request[:MediaId]) # Echo the sent image to user
+  end
+
+  # When user sends a voice
+  on :voice do |request|
+    request.reply.voice(request[:MediaId]) # Echo the sent voice to user
+  end
+
+  # When user sends a video
+  on :video do |request|
+    nickname = wechat.user(request[:FromUserName])['nickname'] # Call wechat api to get sender nickname
+    request.reply.video(request[:MediaId], title: 'Echo', description: "Got #{nickname} sent video") # Echo the sent video to user
+  end
+
+  # When user sends location message with label
+  on :label_location do |request|
+    request.reply.text("Label: #{request[:Label]} Location_X: #{request[:Location_X]} Location_Y: #{request[:Location_Y]} Scale: #{request[:Scale]}")
+  end
+
+  # When user sends location
+  on :location do |request|
+    request.reply.text("Latitude: #{request[:Latitude]} Longitude: #{request[:Longitude]} Precision: #{request[:Precision]}")
+  end
+
+  on :event, with: 'unsubscribe' do |request|
+    request.reply.success # user can not receive this message
+  end
+
+  # When user enters the app / agent app
+  on :event, with: 'enter_agent' do |request|
+    request.reply.text "#{request[:FromUserName]} enter agent app now"
+  end
+
+  # When batch job "create/update user (incremental)" is finished.
+  on :batch_job, with: 'sync_user' do |request, batch_job|
+    request.reply.text "sync_user job #{batch_job[:JobId]} finished, return code #{batch_job[:ErrCode]}, return message #{batch_job[:ErrMsg]}"
+  end
+
+  # When batch job "replace user (full sync)" is finished.
+  on :batch_job, with: 'replace_user' do |request, batch_job|
+    request.reply.text "replace_user job #{batch_job[:JobId]} finished, return code #{batch_job[:ErrCode]}, return message #{batch_job[:ErrMsg]}"
+  end
+
+  # When batch job "invite user" is finished.
+  on :batch_job, with: 'invite_user' do |request, batch_job|
+    request.reply.text "invite_user job #{batch_job[:JobId]} finished, return code #{batch_job[:ErrCode]}, return message #{batch_job[:ErrMsg]}"
+  end
+
+  # When batch job "replace department (full sync)" is finished.
+  on :batch_job, with: 'replace_party' do |request, batch_job|
+    request.reply.text "replace_party job #{batch_job[:JobId]} finished, return code #{batch_job[:ErrCode]}, return message #{batch_job[:ErrMsg]}"
+  end
+
+  # mass sent job finish result notification
+  on :event, with: 'masssendjobfinish' do |request|
+    # https://mp.weixin.qq.com/wiki?action=doc&id=mp1481187827_i0l21&t=0.03571905015619936#8
+    request.reply.success # request is XML result hash.
+  end
+
+  # If no match above will fallback to below
+  on :fallback, respond: 'fallback message'
+end
